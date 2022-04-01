53910652bd05e5e291e5bf705b35f2c20f6be2a1
Vigneshwar <prasannavenkatesh@colanonline.com>
2022-04-01 15:42:15 +0530

    layoutAnimation

--- App.tsx
+++ App.tsx
@@ -1,65 +1,46 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 
-import React from 'react';
+import React from "react";
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import FirstPage from './pages/FirstPage';
-import SecondPage from './pages/SecondPage';
-import PushNotification from './pages/pushNotification';
+import FirstPage from "./pages/FirstPage";
+import SecondPage from "./pages/SecondPage";
+import PushNotification from "./pages/pushNotification";
+import LayoutAnimation_screen from "./pages/LayoutAnimation";
+
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="PushNotification" screenOptions={{
-    headerShown: false
-  }} >
+      <Stack.Navigator
+        initialRouteName="LayoutAnimation_screen"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
         <Stack.Screen
           name="FirstPage"
           component={FirstPage}
           options={{
-            title: 'First Page', //Set Header Title
+            title: "First Page", //Set Header Title
             headerStyle: {
-              backgroundColor: '#f4511e', //Set Header color
+              backgroundColor: "#f4511e", //Set Header color
             },
-            headerTintColor: '#fff', //Set Header text color
+            headerTintColor: "#fff", //Set Header text color
             headerTitleStyle: {
-              fontWeight: 'bold', //Set Header text style
-            },
-            
-          }}
-        />
-        <Stack.Screen
-          name="SecondPage"
-          component={SecondPage}
-          options={{
-            title: 'Second Page', //Set Header Title
-            headerStyle: {
-              backgroundColor: '#f4511e', //Set Header color
-            },
-            headerTintColor: '#fff', //Set Header text color
-            headerTitleStyle: {
-              fontWeight: 'bold', //Set Header text style
+              fontWeight: "bold", //Set Header text style
             },
           }}
         />
-    
-      <Stack.Screen
-          name="PushNotification"
-          component={PushNotification}
-          options={{
-            title: 'PushNotification Page', //Set Header Title
-            headerStyle: {
-              backgroundColor: '#f4511e', //Set Header color
-            },
-            headerTintColor: '#fff', //Set Header text color
-            headerTitleStyle: {
-              fontWeight: 'bold', //Set Header text style
-            },
-          }}
-        />
+        <Stack.Screen name="SecondPage" component={SecondPage} />
+
+        <Stack.Screen name="PushNotification" component={PushNotification} />
+
+        <Stack.Screen name="LayoutAnimation_screen" component={LayoutAnimation_screen} />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
